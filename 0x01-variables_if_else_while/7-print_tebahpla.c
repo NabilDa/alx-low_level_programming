df6616d20632f19e9e93226a26e4504ba083f9dd
Nabil <naabil.daoui@gmail.com>
2023-09-08 20:11:32 +0000

    tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 122;
+
+	while (c >= 97)
+	{
+		putchar(c);
+		c--;
+	}
+	putchar(10);
+
+	return(0);
+}
