b1de533e5bb0dbace1c1a0035a5fc80a125367a0
Nabil <naabil.daoui@gmail.com>
2023-09-12 14:52:13 +0000

    Create a C program that calculates the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Calculates the absolute value of n
+ * @n: The integer to be converted into its absolute value
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
