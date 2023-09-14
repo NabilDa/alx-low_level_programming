16cf8b023f46982bedeee45ed14aa6837e71e31e
Nabil <naabil.daoui@gmail.com>
2023-09-14 08:42:12 +0000

    Fix a function that returns the largest of three integers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+/**
+ * largest_number - returns the largest of 3 integers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ *
+ * Return: largest number
+ */
+int largest_number(int a, int b, in c)
+{
+	int largest;
+
+	if (a < b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
