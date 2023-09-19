b800ac13be54470c3a6cb90a143682b51a229a99
Nabil <naabil.daoui@gmail.com>
2023-09-19 15:53:31 +0000

    Add documentation to the function _puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: the address of the first character of the string
+ * to be printed
+ *
+ * Return: nothing
+ */
 void _puts(char *str)
 {
 	int i;
