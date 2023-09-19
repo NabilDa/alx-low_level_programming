24a6362e50def132b8ffdc2747e17c7ae905206d
Nabil <naabil.daoui@gmail.com>
2023-09-19 14:55:29 +0000

    Create a function that updates the value of an integer to 98

---
+++ 0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - updates the value of an int to 402
+ * @n: pointer to the integer we want to change to 402
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
