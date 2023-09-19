ed825e869b30a312d12a5a9656b1ed566178ef61
Nabil <naabil.daoui@gmail.com>
2023-09-19 16:06:38 +0000

    Add a documentation to the function print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to the first character of the string
+ */
+
 void print_rev(char *s)
 {
 	int i;
