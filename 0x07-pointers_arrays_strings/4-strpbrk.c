2d39bf637389ff32573ed152c28bd2761fd1db22
Nabil <naabil.daoui@gmail.com>
2023-09-26 08:44:43 +0000

    Fix wrong function spelling in the documentation

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strbprk - locate the location in memory of a byte in s from accept
+ * _strpbrk - locate the location in memory of a byte in s from accept
  * @s: the string to locate in
  * @accept: the string from which to compare bytes
  *
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-n	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
