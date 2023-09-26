bf270c85267df0a9049bbfcb105363e0a9dc119f
Nabil <naabil.daoui@gmail.com>
2023-09-26 11:26:48 +0000

    Write a function that sums diagonal

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -5,3 +6,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+void print_diagsums(int *a, int size);
