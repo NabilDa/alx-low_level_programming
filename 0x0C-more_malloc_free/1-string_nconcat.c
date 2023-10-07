01f81bfe56370c512d194fde92b0930142813bc8
Nabil <naabil.daoui@gmail.com>
2023-10-07 18:52:08 +0000

    Fix the case when one of the pointers is NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,33 +15,40 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size_s1, size_s2;
 	char *ptr;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
+		return (NULL);
 		size_s1 = 0;
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		size_s1 = strlen(s1);
+		ptr = malloc(sizeof(char) * size_s1);
+		strcpy(ptr, s1);
+		ptr[size_s1] = '\0';
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		size_s2 = strlen(s2);
+		ptr = malloc(sizeof(char) * size_s2);
+		strncpy(ptr, s2, n);
+		ptr[n] = '\0';
 	}
 	else
 	{
 		size_s1 = strlen(s1);
-	}
-
-	if (s2 == NULL)
-	{
-		size_s2 = 0;
-	}
-	else
-	{
 		size_s2 = strlen(s2);
-	}
 
-	ptr = malloc(sizeof(char) * (size_s1 + size_s2));
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
+		ptr = malloc(sizeof(char) * (size_s1 + size_s2));
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
 
-	strcpy(ptr, s1);
-	strncpy(ptr + size_s1, s2, n);
-	ptr[size_s1 + size_s2] = '\0';
+		strcpy(ptr, s1);
+		strncpy(ptr + size_s1, s2, n);
+		ptr[size_s1 + size_s2] = '\0';
+	}
 
 	return (ptr);
 }
