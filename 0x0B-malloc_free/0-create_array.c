f85a2e918f5b100d272a2be3c529164d2aefc7b5
Nabil <naabil.daoui@gmail.com>
2023-10-08 10:32:10 +0000

    Write a function that creates an array and initializes its all its elements with a character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and initializes its elements with a character
+ * @size: the size of the array
+ * @c: the character to be stored in each element of the array
+ *
+ * Return: if size is 0, return NULL. Otherwise, return array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
