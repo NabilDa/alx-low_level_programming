c887d6361628136cc1ca1e47a4267c26c9dc3135
Nabil <naabil.daoui@gmail.com>
2023-10-09 11:25:02 +0000

    Write a funciton that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,80 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the number of bytes to be taken from s2
+ * allocate_memory - allocates memory
+ * @s1: a string that indicates the size of memory to be allocated
+ * @s2: a string that indicates the additional size of memory to be allocated
+ * @n: if less than s2, it plays the role of s2 on allocation,
+ * otherwise, it's useless
  *
- * Description: concatenates two strings into a pointer that has nothing
- * to do with the two strings, so using malloc is a necessity
- * Return: NULL if malloc fails, and ptr otherwise.
+ * Return: a pointer to the memory allocated.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *allocate_memory(char *s1, char *s2, unsigned int n)
 {
 	unsigned int size_s1, size_s2;
-	char *ptr;
+	char *p;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-		size_s1 = 0;
-	}
-	else if (s1 != NULL && s2 == NULL)
+	if (s1 != NULL)
 	{
 		size_s1 = strlen(s1);
-		ptr = malloc(sizeof(char) * size_s1);
-		strcpy(ptr, s1);
-		ptr[size_s1] = '\0';
-	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		size_s2 = strlen(s2);
-		ptr = malloc(sizeof(char) * size_s2);
-		strncpy(ptr, s2, n);
-		ptr[n] = '\0';
 	}
 	else
 	{
-		size_s1 = strlen(s1);
-		size_s2 = strlen(s2);
-
-		ptr = malloc(sizeof(char) * (size_s1 + size_s2));
-		if (ptr == NULL)
-		{
-			return (NULL);
-		}
-
-		strcpy(ptr, s1);
-		strncpy(ptr + size_s1, s2, n);
-		ptr[size_s1 + size_s2] = '\0';
+		size_s1 = 0;
 	}
 
-	return (ptr);
+	if (s2 != NULL)
+	{
+		size_s2 = strlen(s2);
+	}
+	else
+	{
+		size_s2 = 0;
+	}
+
+	if (n < size_s2)
+	{
+		p = malloc(sizeof(char) * (size_s1 + n + 1));
+	}
+	else
+	{
+		p = malloc(sizeof(char) * (size_s1 + size_s2 + 1));
+	}
+
+	return (p);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the lenght of the string to be taken of s2
+ *
+ * Return: NULL on error, and a pointer to the concatenated string on success.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+
+	p = allocate_memory(s1, s2, n);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	if (s1 != NULL && s2 != NULL)
+	{
+		strcpy(p, s1);
+		strncat(p, s2, n);
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		strcpy(p, s1);
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		strncpy(p, s2, n);
+	}
+
+	return (p);
 }
